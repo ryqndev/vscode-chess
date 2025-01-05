a4e8d0505ede087e3e8fd28432191b2f7d157e7b
Ryan Yang <ryanqyang@gmail.com>
2025-01-05 11:10:32 -0800

    guaranteed to have engine now

--- src/app/src/controllers/Engine.ts
+++ src/app/src/controllers/Engine.ts
@@ -1,36 +1,19 @@
 import SF from './stockfish.wasm.js?raw';
-/**
- * Requires stockfish.wasm.js and stockfish.js to be present in the public/engine folder
- */
-// const STOCKFISH_WASM_PUBLIC_PATH = 'engine/stockfish.wasm.js';
-// const STOCKFISH_JS_FALLBACK_PUBLIC_PATH = 'engine/stockfish.js';
-
-// const HELLO_WASM_TEST_STRING = Uint8Array.of(0x0, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00);
-
 
 class StockfishEngine {
-    engine?: Worker;
+    engine: Worker;
 
     constructor() {
-        // const wasmSupported = typeof WebAssembly === 'object' && WebAssembly.validate(HELLO_WASM_TEST_STRING);
-        // const workerUrl = wasmSupported ? STOCKFISH_WASM_PUBLIC_PATH : STOCKFISH_JS_FALLBACK_PUBLIC_PATH;
-
-        console.log(SF, '@ryqndev');
-
-        // this.engine = new Worker((workerUrl));
         this.engine = new Worker(`data:text/javascript;base64,${btoa(SF)}`);
-
     }
 
     // Inits using UCI and starts new game
     start(positionFen?: string) {
-        if (!this.engine) return;
         this.engine.postMessage('uci');
         this.engine.postMessage('isready');
         this.engine.postMessage('ucinewgame');
 
         this.engine.postMessage('setoption name Skill Level value -1');
-        console.log('@ryqndev set level');
         // this.engine.postMessage('setoption name UCI_Elo value 1320');
 
         this.engine.postMessage(`position ${positionFen ? 'fen ' + positionFen : 'startpos'}`);
@@ -38,8 +21,6 @@ class StockfishEngine {
     }
 
     move(positionFen: string) {
-        if (!this.engine) return;
-
         this.engine.postMessage(`position fen ${positionFen}`);
         this.engine.postMessage(`go`);
     }
@@ -47,8 +28,6 @@ class StockfishEngine {
     // setDifficulty()
 
     listen(onMessage: (message: string) => void) {
-        if (!this.engine) return;
-
         return this.engine.addEventListener('message', event => onMessage(event.data));
     }
 
