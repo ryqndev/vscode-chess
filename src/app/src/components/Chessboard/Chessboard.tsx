bb81145ca153fa5e1bed71a511e2ce472a2aaa1d
Ryan Yang <ryanqyang@gmail.com>
2025-01-05 14:09:34 -0800

    got the chess board logic flow down with puzzles. now just fighting some css

--- src/app/src/components/Chessboard/Chessboard.tsx
+++ src/app/src/components/Chessboard/Chessboard.tsx
@@ -1,61 +1,69 @@
-import { memo, useCallback } from "react";
+import { memo, RefObject, useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { Chessboard as ReactChessBoard } from "react-chessboard";
 import { Chess, PieceSymbol, Square } from "chess.js";
-import { PromotionPieceOption } from "react-chessboard/dist/chessboard/types";
-import { Move } from "./types";
+import { ChessboardProps as ReactChessboardProps, PromotionPieceOption } from "react-chessboard/dist/chessboard/types";
 
-// const game = new Chess();
 
-interface ChessboardProps {
+type ChessboardProps = Omit<ReactChessboardProps, 'ref'> & {
     fen: string;
-    // setFen: (fen: string) => void;
     game: Chess;
+    container: RefObject<HTMLDivElement>;
 }
 
 export const Chessboard = memo(function Chessboard({
-    game,
-    fen
+    fen,
+    container,
+    ...props
 }: ChessboardProps) {
 
-    console.log('@ryqndev', fen);
-    const makeMove = useCallback((move: Move, callback?: () => void) => {
-        try {
-            game.move(move);
-        } catch (e) {
-            return false;
-        }
-        // setFen(game.fen());
-        callback?.();
-    }, [game]);
+    // function onPromotionPieceSelect(
+    //     piece?: PromotionPieceOption,
+    //     promoteFromSquare?: Square,
+    //     promoteToSquare?: Square
+    // ) {
+    //     if (!piece || !promoteFromSquare || !promoteToSquare) return false;
+    //     makeMove(
+    //         {
+    //             from: promoteFromSquare,
+    //             to: promoteToSquare,
+    //             promotion: piece.substring(1).toLowerCase() as PieceSymbol,
+    //         });
 
-    function onDrop(sourceSquare: Square, targetSquare: Square) {
-        makeMove({ from: sourceSquare, to: targetSquare });
-        return true;
-    }
+    //     return true;
+    // }
 
-    function onPromotionPieceSelect(
-        piece?: PromotionPieceOption,
-        promoteFromSquare?: Square,
-        promoteToSquare?: Square
-    ) {
-        if (!piece || !promoteFromSquare || !promoteToSquare) return false;
+    const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
 
-        makeMove(
-            {
-                from: promoteFromSquare,
-                to: promoteToSquare,
-                promotion: piece.substring(1).toLowerCase() as PieceSymbol,
+    useEffect(() => {
+        if (container.current) {
+            const observer = new ResizeObserver((entries) => {
+                for (const entry of entries) {
+                    setDimensions({
+                        width: entry.contentRect.width,
+                        height: entry.contentRect.height,
+                    });
+                }
             });
 
-        return true;
-    }
+            observer.observe(container.current);
+
+            return () => {
+                observer.disconnect();
+            };
+        }
+    }, [container]);
+    const boardWidth = useMemo(() => ~~Math.min(dimensions.width, dimensions.height), [dimensions]);
+
+    console.log('@ryqndev width', ~~boardWidth);
+    if (!boardWidth) return null;
 
     return (
         <ReactChessBoard
             id="main"
             position={fen}
-            onPieceDrop={onDrop}
-            onPromotionPieceSelect={onPromotionPieceSelect}
+            boardWidth={boardWidth}
+            // onPromotionPieceSelect={onPromotionPieceSelect}
+            {...props}
         />
     );
 });
--- src/app/src/pages/Puzzle/controllers/usePuzzle.tsx
+++ src/app/src/pages/Puzzle/controllers/usePuzzle.tsx
@@ -1,34 +1,76 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { Puzzle } from "../types";
 import SAMPLE_PUZZLE_DATA from './SAMPLE_PUZZLE_DATA.json';
 import { Chess } from "chess.js";
+import { Piece, ChessboardProps as ReactChessboardProps, Square } from "react-chessboard/dist/chessboard/types";
+import { Move } from "../../../components/Chessboard/types";
 
 const LICHESS_PUZZLE_API_ENDPOINT = `https://lichess.org/api/puzzle/daily`;
 const debug = true;
 
-const game = new Chess();
+type PuzzleProps = Partial<ReactChessboardProps> & {
+    game: Chess;
+    fen: string;
 
-export const usePuzzle = () => {
-    const [puzzle, setPuzzle] = useState<Puzzle | null>(null);
-    const [fen, setFen] = useState<string>();
+    // intiially undefined, guaranteed value after API call
+    puzzle?: Puzzle;
+}
 
+export const usePuzzle = (): PuzzleProps => {
+    const game = useMemo(() => new Chess(), []);
+
+    // Chess isn't reactive, so will use the fen representation
+    // of the game as the react update. Every time board state changes,
+    // should not only update the game object but also the fen state
+    const [fen, setFen] = useState(() => game.fen());
+
+    const [puzzle, setPuzzle] = useState<Puzzle>();
+    const [moveList, setMoveList] = useState<string[]>();
+
+    const makeMove = useCallback((move: Move) => {
+        game.move(move);
+        setFen(game.fen());
+    }, [game]);
+
+    const onPieceDrop = useCallback((sourceSquare: Square, targetSquare: Square, piece: Piece) => {
+        if (!moveList) return false;
+
+        const move = sourceSquare + targetSquare;
+        const [correctMove, response, ...restOfSequence] = moveList;
+
+        if (move !== correctMove) {
+            // errr! u messed up
+            alert('U made a big mistake buddy');
+            return true;
+        }
+
+        makeMove(move);
+
+        if (!response) {
+            // that was the final correct move
+            alert('passed!');
+            return true;
+        }
+        setTimeout(() => {
+            makeMove(response);
+            setMoveList(restOfSequence);
+        }, 300);
+        return true;
+
+    }, [moveList, makeMove]);
 
     useEffect(() => {
-        if (debug) {
-            setPuzzle(SAMPLE_PUZZLE_DATA);
-
-        } else {
-            fetch(LICHESS_PUZZLE_API_ENDPOINT).then(res => res.json()).then(setPuzzle);
-
-        }
+        if (debug) return setPuzzle(SAMPLE_PUZZLE_DATA);
+        fetch(LICHESS_PUZZLE_API_ENDPOINT).then(res => res.json()).then(setPuzzle);
     }, []);
 
     useEffect(() => {
         if (!puzzle) return;
-        game.loadPgn(puzzle?.game.pgn);
-        console.log('@ryqndev', game.fen());
-        setFen(game.fen());
-    }, [puzzle]);
 
-    return { puzzle, game, fen };
+        game.loadPgn(puzzle?.game.pgn);
+        setMoveList(puzzle.puzzle.solution);
+        setFen(game.fen());
+    }, [puzzle, game]);
+
+    return { puzzle, game, fen, onPieceDrop };
 };
