c446a40d318771764093169e47430e76e232906b
Ryan Yang <ryanqyang@gmail.com>
2025-01-05 16:00:57 -0800

    added historical search

--- src/app/src/components/MoveHistory/MoveHistory.tsx
+++ src/app/src/components/MoveHistory/MoveHistory.tsx
@@ -1,26 +1,40 @@
-import { memo, useMemo } from "react";
+import { memo, useCallback, useMemo } from "react";
 import cn from './MoveHistory.module.scss';
+import { Move } from "chess.js";
 
-export const MoveHistory = memo(function MoveHistory({ history }: { history: string[] }) {
+interface MoveHistoryProps {
+    history: Move[],
+    setHoveredPos: (fen: string | undefined) => void
+}
+
+export const MoveHistory = memo(function MoveHistory({ history, setHoveredPos }: MoveHistoryProps) {
     const parsedHistory = useMemo(() =>
-        history
-            .reduce((acc, _, idx, array) => {
-                if (idx % 2 === 0) {
-                    acc.push(array.slice(idx, idx + 2));
-                }
-                return acc;
-            }, [] as string[][])
-        , [history]);
+        history.reduce((acc, _, idx, array) => {
+            if (idx % 2 === 0) {
+                acc.push(array.slice(idx, idx + 2).map(e => e.san));
+            }
+            return acc;
+        }, [] as string[][])
+        , [history]
+    );
+
+    const displayHoveredPos = useCallback((moveNum: number) => () => {
+        setHoveredPos(history[moveNum].after);
+    }, [history, setHoveredPos]);
+
+    const clearHoveredPos = useCallback(() => {
+        // setHoveredPos(undefined);
+    }, [setHoveredPos]);
 
     return (
         <div className={cn.container}>
             <div className={cn.table}>
                 {
                     parsedHistory.map((turn, idx) =>
-                        <div className={cn.row} key={idx}>
+                        <div className={cn.row} key={idx} onMouseLeave={clearHoveredPos}>
                             <div>{idx + 1}.</div>
-                            <div>{turn[0]}</div>
-                            <div>{turn[1]}</div>
+                            <div onMouseEnter={displayHoveredPos(idx * 2)} >{turn[0]}</div>
+                            <div onMouseEnter={displayHoveredPos(idx * 2 + 1)}>{turn[1]}</div>
                         </div>
                     )
                 }
--- src/app/src/pages/Puzzle/Puzzle.tsx
+++ src/app/src/pages/Puzzle/Puzzle.tsx
@@ -1,5 +1,5 @@
 import cn from "./Puzzle.module.scss";
-import { memo, useRef } from "react";
+import { memo, useMemo, useRef, useState } from "react";
 import { Chessboard } from "../../components/Chessboard/Chessboard.js";
 import { usePuzzle } from "./controllers/usePuzzle.js";
 import { MoveHistory } from "../../components/MoveHistory/MoveHistory.js";
@@ -8,18 +8,20 @@ import clsx from "clsx";
 export const Puzzle = memo(function Puzzle() {
 	const container = useRef<HTMLDivElement>(null);
 	const { game, fen, onPieceDrop, solved, next } = usePuzzle();
-	// const [] = useState();
-	// const { bestMove } = useEngine(fen);
+	const [hoveredPos, setHoveredPos] = useState<string>();
 
-	// const turn = game.turn();
+	// fen is reactive, game is not
+	// eslint-disable-next-line 
+	const history = useMemo(() => game.history({ verbose: true }), [fen]);
+
 	return (
 		<div className={cn.page}>
 			<div className={cn.container}>
 				<div className={cn.board} ref={container}>
-					<Chessboard game={game} fen={fen} onPieceDrop={onPieceDrop} container={container} />
+					<Chessboard game={game} fen={hoveredPos ?? fen} onPieceDrop={onPieceDrop} container={container} />
 				</div>
 				<div className={clsx(cn.moves, solved && cn.solved)}>
-					<MoveHistory history={game.history()} />
+					<MoveHistory history={history} setHoveredPos={setHoveredPos} />
 					<div className={cn.actions}>
 						<button onClick={next}>New Puzzle</button>
 					</div>
